db685eff0d9b086d548b6864e8a56d5e2d9f79ac
cvaz007 <sebastianmoreno301177@correo.itm.edu.co>
2023-12-06 13:16:13 -0500

    maquetacion final: falta time

---
+++ DOM-NAVIDEÑO/js/app.js
@@ -0,0 +1,14 @@
+const contNieve = document.querySelector(".nieve");
+contNieve.innerHTML = "";
+
+const generarNieve = () => {
+  Array.from({ length: 50 }).forEach((element) => {
+    const num = random(11, 26);
+    contNieve.innerHTML += `<span style="--i:${num}"></span>`;
+  });
+};
+
+const random = (inicial, final) =>
+  Math.floor(Math.random() * (final - inicial) + inicial);
+
+generarNieve();
